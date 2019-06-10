5cff7fbc4e415b586ca7da534bacd365ab047d44
kukgini <kukgini@gmail.com>
2019-06-10 13:38:55 +0900

    initial

---
+++ src/Runner.java
@@ -0,0 +1,26 @@
+import pubsub.Event;
+import pubsub.Envelop;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Subscriber<String> subscriber1 = new Subscriber<>(1);
+		Subscriber<String> subscriber2 = new Subscriber<>(2);
+		Subscriber<String> subscriber3 = new Subscriber<>(3);
+		Subscriber<String> subscriber4 = new Subscriber<>(4);
+		
+		Event.operation.subscribe("action#create", subscriber1);
+		Event.operation.subscribe("action#create", subscriber2);
+
+		Event.operation.subscribe("action#update", subscriber3);
+		Event.operation.subscribe("action#delete", subscriber4);
+
+		Envelop<String> envelop1 = new Envelop<>("Create Action");
+		Envelop<String> envelop2 = new Envelop<>("Update Action");
+		Envelop<String> envelop3 = new Envelop<>("Delete Action");
+		
+		Event.operation.publish("action#create", envelop1);
+		Event.operation.publish("action#update", envelop2);
+		Event.operation.publish("action#delete", envelop3);
+	}
+}
---
+++ src/Subscriber.java
@@ -0,0 +1,14 @@
+import pubsub.OnReceived;
+import pubsub.Envelop;
+
+class Subscriber<T> {
+    int id;
+    public Subscriber(int id) {
+        this.id = id;
+    }
+
+    @OnReceived
+    private void onReceived(Envelop<T> envelop) {
+        System.out.printf("Subscriber[%d] received: %s%n", id, envelop.open());
+    }
+}
