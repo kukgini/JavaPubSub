7b522d349031e74f20faa47c28c723ec402ec36d
kukgini <kukgini@gmail.com>
2019-06-11 08:30:52 +0900

    Enable MultiThread.

--- src/Runner.java
+++ src/Runner.java
@@ -1,14 +1,19 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import pubsub.Event;
 import pubsub.Message;
 
 public class Runner {
 
+	private static final ExecutorService executors = Executors.newCachedThreadPool();
+	
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {		
-		Event.on.subscribe("action#create", m -> print(1,m));
-		Event.on.subscribe("action#create", m -> print(2,m));
-		Event.on.subscribe("action#update", m -> print(3,m));
-		Event.on.subscribe("action#delete", m -> print(4,m));
+		Event.on.subscribe("action#create", m -> executors.submit(() -> print(1,m)));
+		Event.on.subscribe("action#create", m -> executors.submit(() -> print(2,m)));
+		Event.on.subscribe("action#update", m -> executors.submit(() -> print(3,m)));
+		Event.on.subscribe("action#delete", m -> executors.submit(() -> print(4,m)));
 
 		Message<String> message1 = new Message<>("Create Action");
 		Message<String> message2 = new Message<>("Update Action");
@@ -20,6 +25,6 @@ public class Runner {
 	}
 	public static void print(int id, Message<String>  m) {
 		String s = m.open();
-		System.out.printf("Subscriber[%d] received: %s%n", id, s);
+		System.out.printf("Subscriber[%d] received: %s on thread[%d]%n", id, s, Thread.currentThread().getId());
 	}
 }
