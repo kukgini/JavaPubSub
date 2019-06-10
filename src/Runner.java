3c912c50022f337a188b00cf43787df3d294ad4d
kukgini <kukgini@gmail.com>
2019-06-10 17:27:36 +0900

    ...

--- src/Runner.java
+++ src/Runner.java
@@ -15,12 +15,12 @@ public class Runner {
 		Event.operation.subscribe("action#update", subscriber3);
 		Event.operation.subscribe("action#delete", subscriber4);
 
-		Message<String> envelop1 = new Message<>("Create Action");
-		Message<String> envelop2 = new Message<>("Update Action");
-		Message<String> envelop3 = new Message<>("Delete Action");
+		Message<String> message1 = new Message<>("Create Action");
+		Message<String> message2 = new Message<>("Update Action");
+		Message<String> message3 = new Message<>("Delete Action");
 		
-		Event.operation.publish("action#create", envelop1);
-		Event.operation.publish("action#update", envelop2);
-		Event.operation.publish("action#delete", envelop3);
+		Event.operation.publish("action#create", message1);
+		Event.operation.publish("action#update", message2);
+		Event.operation.publish("action#delete", message3);
 	}
 }
